4417b07d50b319c21d4eca7a5fb446b5f1031a89
OmarSalhab <omarsalhb24@gmail.com>
2024-11-27 13:47:46 +0300

    minor fixes

--- server.js
+++ server.js
@@ -2,44 +2,35 @@ import express from "express";
 import tesseract from "tesseract.js";
 import path from "path";
 import { fileURLToPath } from "url";
-import multer from 'multer'
+import multer from "multer";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const upload = multer({dest: "uploads/"})
+const upload = multer({ dest: "uploads/" });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/pages/index.html"));
+	res.sendFile(path.join(__dirname, "public/pages/index.html"));
 });
 
 app.listen(port, () => {
-	
 	console.log(`Server is running at http://localhost:${port}`);
 });
 
-app.post("/process-image",upload.single("image"),(req,res) => {
-    
-    const imagePath = req.file.path
-    
-    tesseract
-	.recognize(
-		imagePath,
-		"eng",
-		{ logger: (m) => console.log(m) }
-	)
-	.then(({ data: { text } }) => {
-		res.json({text})
-    }).catch((err) => {
-        console.error(err)
-        res.status(500).json({error: "Failed to process the image"})
-    })
-})
+app.post("/process-image", upload.single("image"), (req, res) => {
+	const imagePath = req.file.path;
 
-
-
+	tesseract
+		.recognize(imagePath, "eng", { logger: (m) => console.log(m) })
+		.then(({ data: { text } }) => {
+			res.json({ text });
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).json({ error: "Failed to process the image" });
+		});
+});
